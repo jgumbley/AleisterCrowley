86fa66e137bdd24eee4e63543bce309d20b7d4ac
Jim Gumbley <jgumbley@thoughtworks.com>
2016-06-21 07:59:50 +0100

    clean up tests

--- test/AleisterCrowley.UnitTest/Test.cs
+++ test/AleisterCrowley.UnitTest/Test.cs
@@ -6,22 +6,14 @@ namespace AleisterCrowley.Test
     public class WizardTests
     {
         [TestCase]
-        public void CanCreateWizard()
-        {
-            WizardJourney wiz = new WizardJourney();
-        }
-
-        public void callNext(WizardJourney wiz) {
-            wiz.nextStepInJourney();
-        }
-
-        [TestCase]
         public void EmptyWizard_EndOfWizardException()
         {
             // Given
             WizardJourney wiz = new WizardJourney();
             // When Then
-            Assert.Throws<EndOfWizardException>(delegate { wiz.nextStepInJourney(); });
-            }
+            Assert.Throws<EndOfWizardException>(delegate { 
+                wiz.nextStepInJourney(); 
+                });
+        }
     }
 }
